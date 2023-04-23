67cf0e293a17f25dd7fc034365b29c7494af1458
Boniel-M <bonielwanjohi@gmail.com>
2023-04-23 04:17:44 -0700

    _printf

---
+++ 0-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int c = 21;
+	char *str = "BonielChemu";
+
+	printf("%d\n", c);
+	printf("%s\n", str);
+	printf("%%\n");
+
+	return (0);
+}
---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * _printf - function that produces an output according format
+ * @format: string to be formatted
+ * Return: number of char printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	int counter = 0;
+	char c;
+	char *s;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == 'c')
+			{
+				c = (char) va_arg(args, int);
+				putchar(c);
+				counter++;
+			}
+			else if (*format == 's')
+			{
+				s = va_arg(args, char *);
+				puts(s);
+				counter += strlen(s);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				counter++;
+			}
+			else
+			{
+				putchar(*format);
+				counter++;
+			}
+			format++;
+		}
+	}
+	va_end(args);
+	return (counter);
+}
