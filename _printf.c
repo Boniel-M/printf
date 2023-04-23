6e3be9df5ff08abcc1857d2bf0827b58fb84fbc2
Boniel-M <bonielwanjohi@gmail.com>
2023-04-23 06:28:49 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,58 +1,63 @@
-#include "main.h"
-#include <string.h>
-#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 /**
- * _printf - function that produces an output according format
- * @format: string to format.
- * Return: number of char printed
+ * _printf - Produces output according to a format
+ * @format: The format string
+ * Return: The number of characters printed
  */
-int _putchar(char c)
-{
-	return (putchar(c));
-}
 int _printf(const char *format, ...)
 {
-	va_list args;
+        va_list args;
+        int i = 0, j = 0, len = 0;
+        char *str;
 
-	int count = 0;
-	char c;
+        va_start(args, format);
 
-	va_start(args, format);
+        while (format[i] != '\0')
+        {
+                if (format[i] == '%')
+                {
+                        i++;
+                        switch (format[i])
+                        {
+                                case 'c':
+                                        len += write(1, (char)va_arg(args, int), 1);
+                                        break;
+                                case 's':
+                                        str = va_arg(args, char *);
+                                        while (str[j] != '\0')
+                                        {
+                                                len += write(1, &str[j], 1);
+                                                j++;
+                                        }
+                                        break;
+                                case '%':
+                                        len += write(1, "%", 1);
+                                        break;
+                                default:
+                                        len += write(1, "%", 1);
+                                        len += write(1, &format[i], 1);
+                                        break;
+                        }
+                }
+                else
+                {
+                        len += write(1, &format[i], 1);
+                }
+                i++;
+        }
 
-	while ((c = *format++))
-	{
-		if (c == '%')
-		{
-			c = *format++;
-			if (c == 'c')
-			{
-				count += putchar(va_arg(args, int));
-			}
-			else if (c == 's')
-			{
-				char *str = va_arg(args, char *);
-				
-				while (*str)
-				{
-					count += putchar(*str++);
-				}
-			}
-			else if (c == '%')
-			{
-				count += putchar('%');
-			}
-			else
-			{
-				count += putchar('%');
-				count += putchar(c);
-			}
-		}
-		else
-		{
-			count += putchar(c);
-		}
-	}
-	va_end(args);
-	return count;
+        va_end(args);
+
+        return len;
 }
+
+int main(void)
+{
+        _printf("Hello, %s! %c %%%s\n", "world", '!', "It works!");
+
+        return (0);
+}
+
