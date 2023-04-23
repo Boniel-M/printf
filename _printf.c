a501109adee6acdd4be050b9a31b72ec862c7f1d
Boniel-M <bonielwanjohi@gmail.com>
2023-04-23 07:18:35 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -1,28 +1,35 @@
 #include <stdarg.h>
 #include <stdio.h>
-
+/**
+ * _printf - prints a formatted output
+ * @format: the format string
+ *
+ * Return: number of characters printed, or -1 on error.
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	
+
 	va_start(args, format);
-	
+
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			
+
 			if (*format == 'c')
 			{
 				char c = (char) va_arg(args, int);
+
 				putchar(c);
 				count++;
 			}
 			else if (*format == 's')
 			{
 				char *str = va_arg(args, char *);
+
 				while (*str)
 				{
 					putchar(*str);
@@ -48,5 +55,5 @@ int _printf(const char *format, ...)
 		format++;
 	}
 	va_end(args);
-	return count;
+	return (count);
 }
