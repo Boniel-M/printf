467a6e2445b2d951cb3b2d815312caa4210e8e3f
Boniel-M <bonielwanjohi@gmail.com>
2023-04-26 01:40:44 -0700

    print function

--- 0-printf_function.c
+++ 0-printf_function.c
@@ -1,73 +1,55 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
 
-/**
- * The `_printf()` function formats and prints data to the standard output stream
- *
- * @param format - The format string that contains zero or more directives
- * @param ... - The optional arguments to be formatted
- * @return The number of characters printed (excluding the null byte used to end output to strings)
- *
- * The format string can contain conversion specifiers:
- * - %c - Character
- * - %s - String
- * - %% - Percent character
- *
- * Unsupported conversion specifiers will result in undefined behavior.
- * Field width and precision are not supported in this implementation.
- */
-int _printf(const char *format, ...) {
-    int count = 0;
+int _printf(const char *format, ...)
+{
     va_list args;
+    int count = 0;
+    char c;
+
     va_start(args, format);
-    while (*format != '\0') {
-        if (*format == '%') {
+
+    while ((c = *format++))
+    {
+        if (c == '%')
+        {
             format++;
-            switch (*format) {
-                case 'c': {
-                    char c = (char) va_arg(args, int);
-                    putchar(c);
+
+            switch (*format)
+            {
+                case 'c':
+                {
+                    char ch = (char) va_arg(args, int);
+                    putchar(ch);
                     count++;
                     break;
                 }
-                case 's': {
-                    const char *s = va_arg(args, const char *);
-                    while (*s != '\0') {
-                        putchar(*s);
-                        s++;
+                case 's':
+                {
+                    char *str = va_arg(args, char *);
+                    while (*str)
+                    {
+                        putchar(*str++);
                         count++;
                     }
                     break;
                 }
-                case '%': {
+                case '%':
+                {
                     putchar('%');
                     count++;
                     break;
                 }
-                default: {
-                    /* unsupported conversion specifier */
-                    va_end(args);
-                    return (-1);
-                }
             }
-        } else {
-            putchar(*format);
+        }
+        else
+        {
+            putchar(c);
             count++;
         }
-        format++;
     }
-    va_end(args);
-    return (count);
-}
 
-int main() {
-    int num_chars;
-    num_chars = _printf("Testing _printf() function in C\n");
-    num_chars = _printf("Character: %c\n", 'a');
-    num_chars = _printf("String: %s\n", "Hello, world!");
-    num_chars = _printf("Percent sign: %%\n");
-    num_chars = _printf("The number of characters printed is: %d\n", 42);
-    num_chars = _printf("The total number of characters printed is: %d\n", num_chars);
-    return (0);
+    va_end(args);
+    return count;
 }
