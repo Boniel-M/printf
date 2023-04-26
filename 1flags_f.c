81cceb8ac1c5b44818971096f6bfef32abff3da0
Boniel-M <bonielwanjohi@gmail.com>
2023-04-26 05:32:40 -0700

    c files

--- 1flags_f.c
+++ 1flags_f.c
@@ -1,88 +1,57 @@
 #include <stdarg.h>
-#include <stdio.h>
-#include "common_functions.h"
 #include "main.h"
+#include <stdio.h>
 
-/**
- * print_string - Prints a string.
- * @str: The string to print.
- *
- * Return: The number of characters printed.
- */
-int print_string(char *str)
-{
-	int len = 0;
+int _printf(const char *format, ...) {
+    int num_chars_printed = 0;
+    va_list args;
+    va_start(args, format);
 
-	while (*str)
-	{
-		putchar(*str++);
-		len++;
-	}
-	return (len);
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    {
+                        char c = va_arg(args, int);
+                        putchar(c);
+                        num_chars_printed++;
+                        break;
+                    }
+                case 's':
+                    {
+                        char *str = va_arg(args, char *);
+                        while (*str != '\0') {
+                            putchar(*str);
+                            str++;
+                            num_chars_printed++;
+                        }
+                        break;
+                    }
+                case '%':
+                    {
+                        putchar('%');
+                        num_chars_printed++;
+                        break;
+                    }
+                case 'd':
+                case 'i':
+                    {
+                        int num = va_arg(args, int);
+                        printf("%d", num);
+                        num_chars_printed++;
+                        break;
+                    }
+            }
+        } else {
+            putchar(*format);
+            num_chars_printed++;
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return num_chars_printed;
 }
-/**
- * print_number - Prints an integer.
- * @n: The integer to print.
- *
- * Return: The number of digits printed.
- */
-int print_number(int n)
-{
-	int len = 0;
 
-	if (n < 0)
-	{
-		putchar('-');
-		len++;
-		n = -n;
-	}
-	if (n / 10)
-		len += print_number(n / 10);
-	putchar((n % 10) + '0');
-	len++;
-	return (len);
-}
-/**
- * _printf - Prints a formatted string.
- * @format: The format string to print.
- *
- * Return: The number of characters printed.
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int len = 0;
-
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 's':
-					len += print_string(va_arg(args, char *));
-					break;
-				case 'd':
-				case 'i':
-					len += print_number(va_arg(args, int));
-					break;
-				default:
-					putchar('%');
-					putchar(*format);
-					len += 2;
-					break;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			len++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (len);
-}
