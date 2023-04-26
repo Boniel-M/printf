5f9a0561662355906d5ec26addfd979197a5f006
Boniel-M <bonielwanjohi@gmail.com>
2023-04-26 04:14:42 -0700

    1flags_f.c

--- 1flags_f.c
+++ 1flags_f.c
@@ -10,15 +10,15 @@
  */
 int print_string(char *str)
 {
-    int len = 0;
-    while (*str)
-    {
-        putchar(*str++);
-        len++;
-    }
-    return len;
-}
+	int len = 0;
 
+	while (*str)
+	{
+		putchar(*str++);
+		len++;
+	}
+	return (len);
+}
 /**
  * print_number - Prints an integer.
  * @n: The integer to print.
@@ -27,20 +27,20 @@ int print_string(char *str)
  */
 int print_number(int n)
 {
-    int len = 0;
-    if (n < 0)
-    {
-        putchar('-');
-        len++;
-        n = -n;
-    }
-    if (n / 10)
-        len += print_number(n / 10);
-    putchar((n % 10) + '0');
-    len++;
-    return len;
-}
+	int len = 0;
 
+	if (n < 0)
+	{
+		putchar('-');
+		len++;
+		n = -n;
+	}
+	if (n / 10)
+		len += print_number(n / 10);
+	putchar((n % 10) + '0');
+	len++;
+	return (len);
+}
 /**
  * _printf - Prints a formatted string.
  * @format: The format string to print.
@@ -49,37 +49,39 @@ int print_number(int n)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int len = 0;
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 's':
-                    len += print_string(va_arg(args, char *));
-                    break;
-                case 'd':
-                case 'i':
-                    len += print_number(va_arg(args, int));
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    len += 2;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            len++;
-        }
-        format++;
-    }
-    va_end(args);
-    return len;
+	va_list args;
+	int len = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 's':
+					len += print_string(va_arg(args, char *));
+					break;
+				case 'd':
+				case 'i':
+					len += print_number(va_arg(args, int));
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					len += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			len++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (len);
 }
