4577fa669feb78319b1a43e647f128d5c2e69da3
Boniel-M <bonielwanjohi@gmail.com>
2023-04-26 05:42:55 -0700

    c files

--- 1flags_f.c
+++ 1flags_f.c
@@ -1,16 +1,19 @@
 #include <stdarg.h>
-#include "main.h"
 #include <stdio.h>
 
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     int num_chars_printed = 0;
     va_list args;
     va_start(args, format);
 
-    while (*format != '\0') {
-        if (*format == '%') {
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
             format++;
-            switch (*format) {
+            switch (*format)
+            {
                 case 'c':
                     {
                         char c = va_arg(args, int);
@@ -21,7 +24,8 @@ int _printf(const char *format, ...) {
                 case 's':
                     {
                         char *str = va_arg(args, char *);
-                        while (*str != '\0') {
+                        while (*str != '\0')
+                        {
                             putchar(*str);
                             str++;
                             num_chars_printed++;
@@ -38,12 +42,45 @@ int _printf(const char *format, ...) {
                 case 'i':
                     {
                         int num = va_arg(args, int);
-                        printf("%d", num);
-                        num_chars_printed++;
+                        num_chars_printed += printf("%d", num);
+                        break;
+                    }
+                case 'u':
+                    {
+                        unsigned int num = va_arg(args, unsigned int);
+                        num_chars_printed += printf("%u", num);
+                        break;
+                    }
+                case 'o':
+                    {
+                        unsigned int num = va_arg(args, unsigned int);
+                        num_chars_printed += printf("%o", num);
+                        break;
+                    }
+                case 'x':
+                case 'X':
+                    {
+                        unsigned int num = va_arg(args, unsigned int);
+                        num_chars_printed += printf((*format == 'x') ? "%x" : "%X", num);
+                        break;
+                    }
+                case 'p':
+                    {
+                        void *addr = va_arg(args, void *);
+                        num_chars_printed += printf("%p", addr);
+                        break;
+                    }
+                default:
+                    {
+                        putchar('%');
+                        putchar(*format);
+                        num_chars_printed += 2;
                         break;
                     }
             }
-        } else {
+        }
+        else
+        {
             putchar(*format);
             num_chars_printed++;
         }
@@ -54,4 +91,3 @@ int _printf(const char *format, ...) {
 
     return num_chars_printed;
 }
-
